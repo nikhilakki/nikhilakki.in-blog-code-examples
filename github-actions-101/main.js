58fd899e634e98c9210931a7057d2098478ea845
Nikhil Akki <17634231+nikhilakki@users.noreply.github.com>
2023-02-25 13:00:02 +0530

    sample node app with github actions workflow

---
+++ github-actions-101/main.js
@@ -0,0 +1,9 @@
+"use strict";
+// Copyright (c) 2023 Nikhil Akki
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+exports.__esModule = true;
+var nanoid = require("nanoid");
+var id = nanoid.nanoid();
+console.log("A unique Hello from ".concat(id, " generated on GitHub Actions Server!"));
---
+++ github-actions-101/main.ts
@@ -0,0 +1,10 @@
+// Copyright (c) 2023 Nikhil Akki
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+import * as nanoid from 'nanoid';
+
+const id = nanoid.nanoid();
+
+console.log(`A unique Hello from ${id} generated on GitHub Actions Server!`);
