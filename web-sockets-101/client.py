129e34e615509847f3206eef348a0ae58c88e70f
Nikhil Akki <17634231+nikhilakki@users.noreply.github.com>
2023-05-12 09:20:30 +0530

    updated

--- web-sockets-101/client.py
+++ web-sockets-101/client.py
@@ -1,16 +1,13 @@
 import asyncio
 import websockets
 
-async def hello():
-    uri = "ws://localhost:8765"
-    async with websockets.connect(uri) as websocket:
-        number = int(input("How many itterations? "))
-        for i in range(number):
-            await websocket.send(str(i))
-            print(f">>> Sending {i}")
-
-            activity = await websocket.recv()
-            print(f"<<< Received {activity}")
+async def send_messages():
+    async with websockets.connect("ws://localhost:8765") as websocket:
+        while True:
+            message = input("Type a message to send to the server: ")
+            await websocket.send(message)
+            response = await websocket.recv()
+            print(f"Received response: {response}")
 
 if __name__ == "__main__":
-    asyncio.run(hello())
+    asyncio.run(send_messages())
--- web-sockets-101/server.py
+++ web-sockets-101/server.py
@@ -1,18 +1,14 @@
 import asyncio
 import websockets
 
-async def hello(websocket):
-    answer = await websocket.recv()
-    print(f"<<< {answer}")
-
-    activity = f"I'm thinking... {answer}!"
-
-    await websocket.send(activity)
-    print(f">>> {activity}")
+async def handler(websocket, path):
+    async for message in websocket:
+        print(f"Received message: {message}")
+        await websocket.send(f"Server received your message: {message}")
 
 async def main():
-    async with websockets.serve(hello, "localhost", 8765):
-        await asyncio.Future()  # run forever
+    async with websockets.serve(handler, "localhost", 8765):
+        await asyncio.Future()
 
 if __name__ == "__main__":
     asyncio.run(main())
